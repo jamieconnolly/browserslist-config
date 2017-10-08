981f8d49ed56fa946d363d82d5f3316160e3068d
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-10-08 18:34:31 +0100

    :wrench: Set the defaults

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
 
-module.exports = [];
+module.exports = [
+  '> 5%',
+  'last 2 chrome versions',
+  'last 2 edge versions',
+  'last 2 firefox versions',
+  'last 2 safari versions',
+  'ie 11',
+];
